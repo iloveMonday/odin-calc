f7fbd9b7235656fc09d55f35060df027cb0b5c66
IloveMonday <iloveyouchandlerbing@gmail.com>
2024-02-12 21:45:04 -0500

    Layout in divs - Bad Idea

---
+++ script.js
@@ -0,0 +1,46 @@
+let one = document.getElementById('#one')
+let two = document.getElementById('#two')
+let three = document.getElementById('#three')
+let four = document.getElementById('#four')
+let five = document.getElementById('#five')
+let six = document.getElementById('#six')
+let seven = document.getElementById('#seven')
+let eight = document.getElementById('#eight')
+let nine = document.getElementById('#nine')
+let zero = document.getElementById('#zero')
+let decimal = document.getElementById('#decimal')
+
+let number = document.getElementById('#number-box')
+let buttongrid = document.getElementById('#button-grid')
+let = document.getElementById('#')
+let = document.getElementById('#')
+let = document.getElementById('#')
+
+
+
+
+// function add(a, b) {
+//     return a + b;
+// };
+
+// function subtract(a, b) {
+// return a - b;	
+// };
+
+// function sum(array) {
+// return array.reduce((total, current) => total + current, 0);
+// };
+
+// function multiply(array) {
+// return array.reduce((product, current) => product * current);
+// };
+
+
+// number.addEventListener("click", (e) =>{
+//     e.target.style.backgroundColor = 'red';
+//     console.log("sup")
+//   });
+
+  buttongrid.onmouseover = (e) => {
+    e.target.style.backgroundColor = "black";
+  }
